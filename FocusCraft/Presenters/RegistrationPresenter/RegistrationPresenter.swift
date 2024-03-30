81f384f5d88c86fe3ceb83cf73d12ab4677a95a2
Alibek_Esetov <ablaiyli01@gmail.com>
2024-03-30 22:59:34 +0500

    registration presenter is done

---
+++ FocusCraft/Presenters/RegistrationPresenter/RegistrationPresenter.swift
@@ -0,0 +1,20 @@
+//
+//  RegistrationPresenter.swift
+//  FocusCraft
+//
+//  Created by Алибек Аблайулы on 30.03.2024.
+//
+
+import UIKit
+
+class RegistrationPresenter: RegistrationPresenterProtocol {
+    
+    var view: RegistrationViewProtocol
+    
+    init(view: RegistrationViewProtocol) {
+        self.view = view
+    }
+    
+    
+    
+}
---
+++ FocusCraft/Presenters/RegistrationPresenter/RegistrationPresenterProtocols.swift
@@ -0,0 +1,17 @@
+//
+//  RegistrationProtocol.swift
+//  FocusCraft
+//
+//  Created by Алибек Аблайулы on 30.03.2024.
+//
+
+import UIKit
+
+protocol RegistrationPresenterProtocol {
+    
+}
+
+protocol RegistrationViewProtocol {
+    var presenter: RegistrationPresenterProtocol? { get set }
+    
+}
